74e0127ad30c1eafa4f1bfde3f7bc92003b60793
jamesjlv <james.leal2@gmail.com>
2022-04-16 11:11:33 -0300

    feat: add usecase of movie trending

---
+++ src/data/usecases/movie/remote-movie-trending.ts
@@ -0,0 +1,24 @@
+import { IGetMovieTrending } from "@/domain/usecases/movies/get/remote-get-trending-movie";
+import { IMovieTrending } from "@/domain/models/movies";
+import { HttpStatusCode, HttpMethod, HttpClient } from "@/data/protocols/http";
+
+export class RemoteMovieTrending implements IGetMovieTrending {
+  constructor(
+    private readonly url: string,
+    private readonly httpGetClient: HttpClient<IMovieTrending[]>
+  ) {}
+
+  async exec(param?: string): Promise<IMovieTrending[]> {
+    const httpResponse = await this.httpGetClient.request({
+      url: this.url,
+      method: HttpMethod.Get,
+    });
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return httpResponse.body as IMovieTrending[];
+      default:
+        throw new Error("Não conectou na API");
+    }
+  }
+}
