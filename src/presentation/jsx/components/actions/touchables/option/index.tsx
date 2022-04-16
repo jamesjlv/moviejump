8b400c5d05d35e908ee2b983e372aa6ee52816cf
jamesjlv <james.leal2@gmail.com>
2022-04-16 11:09:59 -0300

    feat: add option move to use in gender

--- src/presentation/jsx/components/actions/touchables/option/index.tsx
+++ src/presentation/jsx/components/actions/touchables/option/index.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { OptionPropsComponent } from "./props";
 
 import { Option as OptionContainer, OptionLabel } from "./styles";
 
 export function Option({ selected = false, label }: OptionPropsComponent) {
+  const [isSelected, setIsSelected] = useState(false);
   return (
-    <OptionContainer selected={selected}>
+    <OptionContainer
+      selected={isSelected}
+      onPress={() => setIsSelected((prevState) => !prevState)}
+    >
       <OptionLabel>{label}</OptionLabel>
     </OptionContainer>
   );
