ab8018cb595aa53469b096f735c40f81c42edbea
jamesjlv <james.leal2@gmail.com>
2022-04-15 21:22:39 -0300

    feat: add gesture handler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { registerRootComponent } from 'expo';
+import "react-native-gesture-handler";
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import { App } from "./src/presentation/jsx/modules/app";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
