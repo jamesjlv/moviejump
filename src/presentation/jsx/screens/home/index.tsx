ea20dcfcb1e37a03b2a16d18692831a0aa4a5199
jamesjlv <james.leal2@gmail.com>
2022-04-15 21:24:59 -0300

    feat: add home screen

--- src/presentation/jsx/screens/home/index.tsx
+++ src/presentation/jsx/screens/home/index.tsx
@@ -1,7 +1,45 @@
 import React from "react";
-
-import { Container } from "./styles";
+import { StatusBar } from "react-native";
+import { Option } from "../../components/actions/touchables/option";
+import { Card } from "../../components/card";
+import {
+  Container,
+  OptionsWrapper,
+  SearchButton,
+  Title,
+  Content,
+  CardsWrapper,
+  SubTitle,
+} from "./styles";
 
 export const Home: React.FC = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <StatusBar barStyle="dark-content" />
+      <SearchButton />
+      <Title>Movies</Title>
+      <OptionsWrapper>
+        <Option label="Popular" selected />
+        <Option label="Comedy" />
+        <Option label="Action" />
+        <Option label="Anime" />
+        <Option label="Love" />
+      </OptionsWrapper>
+      <Content>
+        <CardsWrapper>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </CardsWrapper>
+        <SubTitle>Trending</SubTitle>
+        <CardsWrapper>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </CardsWrapper>
+      </Content>
+    </Container>
+  );
 };
--- src/presentation/jsx/screens/home/styles.ts
+++ src/presentation/jsx/screens/home/styles.ts
@@ -1,3 +1,44 @@
 import styled from "styled-components/native";
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from "react-native-iphone-x-helper";
+import Feather from "react-native-vector-icons/Feather";
 
-export const Container = styled.View``;
+export const Container = styled.ScrollView`
+  flex: 1;
+  flex-direction: column;
+  padding: ${getStatusBarHeight() + 40}px 16px 16px 16px;
+`;
+
+export const SearchButton = styled(Feather).attrs({
+  name: "search",
+  size: 24,
+})``;
+
+export const Title = styled.Text`
+  font-size: 36px;
+  font-weight: 700;
+  margin: 16px 0px;
+`;
+export const SubTitle = styled.Text`
+  font-size: 24px;
+  font-weight: 700;
+  margin: 16px 0px;
+`;
+
+export const OptionsWrapper = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})``;
+
+export const CardsWrapper = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})``;
+
+export const Content = styled.View`
+  /* flex: 1; */
+  margin-top: 32px;
+  padding-bottom: ${getBottomSpace() + 48}px;
+`;
