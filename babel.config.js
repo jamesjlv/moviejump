03a4d867c14b59c8f99ade4d8e6fc3e630fbae76
jamesjlv <james.leal2@gmail.com>
2022-04-16 11:12:00 -0300

    chore: config of project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ts", ".tsx", ".jsx", ".js", ".json"],
+          alias: {
+            "@domain": "./src/domain",
+            "@": "./src/",
+          },
+        },
+      ],
+      "optional-require",
+    ],
   };
 };
