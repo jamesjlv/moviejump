441fe9578416c38957b9513a470677798acf5376
jamesjlv <james.leal2@gmail.com>
2022-04-16 12:42:25 -0300

    chore: handle if genren is selected

--- src/presentation/jsx/components/actions/touchables/option/index.tsx
+++ src/presentation/jsx/components/actions/touchables/option/index.tsx
@@ -3,12 +3,40 @@ import { OptionPropsComponent } from "./props";
 
 import { Option as OptionContainer, OptionLabel } from "./styles";
 
-export function Option({ selected = false, label }: OptionPropsComponent) {
-  const [isSelected, setIsSelected] = useState(false);
+export function Option({
+  selected,
+  label,
+  slug,
+  gendersSelecteds = "",
+  executeWithOnPress,
+  ...rest
+}: OptionPropsComponent) {
+  const [isSelected, setIsSelected] = useState<boolean>(
+    gendersSelecteds.includes(slug)
+  );
+
+  function handleOptionSelected() {
+    setIsSelected((prevState) => !prevState);
+    executeWithOnPress((prevState) => {
+      if (!prevState) {
+        return slug;
+      }
+
+      return handleSlug(prevState, slug);
+    });
+  }
+
+  function handleSlug(prevState: string, newSlug: string): string {
+    let slugs = [slug] as string[];
+    prevState?.split(",").map((slug) => slug != newSlug && slugs.push(slug));
+    return slugs.join(",");
+  }
+
   return (
     <OptionContainer
       selected={isSelected}
-      onPress={() => setIsSelected((prevState) => !prevState)}
+      onPress={handleOptionSelected}
+      {...rest}
     >
       <OptionLabel>{label}</OptionLabel>
     </OptionContainer>
--- src/presentation/jsx/components/actions/touchables/option/props.ts
+++ src/presentation/jsx/components/actions/touchables/option/props.ts
@@ -1,6 +1,11 @@
-export type OptionPropsComponent = {
+import { TouchableOpacityProps } from "react-native";
+
+export type OptionPropsComponent = TouchableOpacityProps & {
   selected?: boolean;
   label: string;
+  slug: string;
+  gendersSelecteds?: string;
+  executeWithOnPress: React.Dispatch<React.SetStateAction<string | undefined>>;
 };
 
 export type OptionProps = {
