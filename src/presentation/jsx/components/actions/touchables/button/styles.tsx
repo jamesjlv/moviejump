58a06fd49bacd4ce6c2f62d70c8143933762da47
jamesjlv <james.leal2@gmail.com>
2022-04-16 13:34:19 -0300

    refactor: ajust styles of button

--- src/presentation/jsx/components/actions/touchables/button/styles.tsx
+++ src/presentation/jsx/components/actions/touchables/button/styles.tsx
@@ -1,21 +1,28 @@
 import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
-import Feather from "react-native-vector-icons/Feather";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled, { css } from "styled-components/native";
+import { IconComponent } from "../icon";
 
 export const Container = styled(TouchableOpacity).attrs({
   activeOpacity: Number(0.7),
 })`
-  font-size: 16px;
   background-color: ${({ theme }) => theme.colors.primary};
-  padding: 16px;
+  padding: ${RFValue(16)}px;
   flex-direction: row;
   align-items: center;
   justify-content: center;
 `;
 
-export const Description = styled.Text``;
+export const Description = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.typography.fontSize.xm}px;
+    font-weight: ${theme.typography.fontFamily.semiBold};
+    line-height: ${theme.typography.lineHeight.xm}px;
+    color: ${theme.colors.black};
+  `}
+`;
 
-export const Arrow = styled(Feather).attrs({
+export const Arrow = styled(IconComponent).attrs({
   name: "chevrons-right",
   size: 12,
 })`
