aed4c435f89843494d118c36b07ce0125913e3f1
jamesjlv <james.leal2@gmail.com>
2022-04-17 12:57:26 -0300

    chore: ignore type on theme provider

--- src/presentation/jsx/modules/theme/index.tsx
+++ src/presentation/jsx/modules/theme/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { Theme } from "../../../styles/theme";
-
+//@ts-ignore
 export function AppTheme({ children }) {
   return <ThemeProvider theme={Theme}>{children}</ThemeProvider>;
 }
