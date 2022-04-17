98133770ebbf9587217c58036bc48cc40a0f4665
jamesjlv <james.leal2@gmail.com>
2022-04-17 12:58:07 -0300

    feat: add mock http cient

---
+++ src/data/test/mock-http-client.ts
@@ -0,0 +1,18 @@
+import {
+  HttpClient,
+  HttpGetClientParams,
+  HttpResponse,
+  HttpStatusCode,
+} from "../protocols/http";
+
+export class HttpClientSpy<T> implements HttpClient<T> {
+  url?: string;
+  response: HttpResponse<T> = {
+    statusCode: HttpStatusCode.ok,
+  };
+
+  async request(params?: HttpGetClientParams<T>): Promise<HttpResponse<T>> {
+    this.url = params?.url;
+    return this.response;
+  }
+}
