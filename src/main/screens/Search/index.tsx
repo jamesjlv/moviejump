6a11adaf5d579bb84e5feb04891ecb8c87b4d5e6
jamesjlv <james.leal2@gmail.com>
2022-04-23 00:59:14 -0300

    FEAT: add search screen to route

--- src/main/screens/Search/index.tsx
+++ src/main/screens/Search/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { SearchScreen } from "@/src/presentation/jsx/screens/search";
 import { manufactureGetRemoteSearchMovie } from "../../services/movie/remote-get-movie-search";
+import { manufactureGetRemoteImageMovies } from "../../services/movie/remote-get-movie-image";
 
-export const manufactureSearchScreen: React.FC = () => (
-  <SearchScreen getMoviesFiltered={manufactureGetRemoteSearchMovie()} />
+export const ManufactureSearchScreen: React.FC = () => (
+  <SearchScreen
+    getMoviesFiltered={manufactureGetRemoteSearchMovie()}
+    getMovieImage={manufactureGetRemoteImageMovies()}
+  />
 );
